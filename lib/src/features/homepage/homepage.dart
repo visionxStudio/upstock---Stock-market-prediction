3494f58250064ba9dadb1523f4da26158aab8881
officialvisionx <manish.rajak2055@gmail.com>
2022-05-31 23:23:36 +0545

    nepse description and top stories added

--- lib/src/features/homepage/homepage.dart
+++ lib/src/features/homepage/homepage.dart
@@ -9,6 +9,7 @@ import 'package:upstock/src/features/homepage/bloc/home_page_notifier.dart';
 import 'package:upstock/src/features/homepage/models/chart_data/chart_data_list/chart_data_list.dart';
 import 'package:upstock/src/features/homepage/models/nepse_stock_model.dart';
 import 'package:upstock/src/features/homepage/widgets/nepse_chart.dart';
+import 'package:upstock/src/features/homepage/widgets/nepse_description.dart';
 
 import '../../common/utils/app_size_utils.dart';
 
@@ -24,21 +25,6 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
   void initState() {
     // ref.read(nepseProvider).getNepseStockData();
     super.initState();
-  }
-
-  String kmbGenerator(String num) {
-    int num1 = double.parse(num).toInt();
-    if (num1 > 999 && num1 < 99999) {
-      return "${(num1 / 1000).toStringAsFixed(1)} K";
-    } else if (num1 > 99999 && num1 < 999999) {
-      return "${(num1 / 1000).toStringAsFixed(0)} K";
-    } else if (num1 > 999999 && num1 < 999999999) {
-      return "${(num1 / 1000000).toStringAsFixed(1)} M";
-    } else if (num1 > 999999999) {
-      return "${(num1 / 1000000000).toStringAsFixed(1)} B";
-    } else {
-      return num1.toString();
-    }
   }
 
   @override
@@ -130,124 +116,20 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
           ),
           const NepseTimeIntervalWidget(),
           const HeightWidget(16.0),
+          const NepseDescription(),
+          const HeightWidget(16.0),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16.0),
             child: SizedBox(
               width: double.infinity,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const NormalText(
-                    "Market Stats",
+                children: const [
+                  NormalText(
+                    "Top Stories",
                     fontSize: kDefaultFontSize + 6,
                     fontWeight: FontWeight.bold,
                   ),
-                  const HeightWidget(8.0),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const [
-                          Icon(
-                            FlutterRemix.funds_box_line,
-                            color: kbluePrimaryColor,
-                          ),
-                          WidthWidget(8.0),
-                          NormalText(
-                            'Nepse Index ',
-                            fontSize: kDefaultFontSize + 2,
-                            fontWeight: FontWeight.bold,
-                            color: kgreyTextColor,
-                          ),
-                        ],
-                      ),
-                      const HeightWidget(kDefaultFontSize * 2),
-                      NepseStockModel.fromStorage() == null
-                          ? const NormalText("")
-                          : NormalText(
-                              NepseStockModel.fromStorage()!
-                                  .closingPrice[NepseStockModel.fromStorage()!
-                                          .closingPrice
-                                          .length -
-                                      1]
-                                  .toString(),
-                              fontSize: kDefaultFontSize + 2,
-                              fontWeight: FontWeight.bold,
-                            )
-                    ],
-                  ),
-                  const HeightWidget(8.0),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const [
-                          Icon(
-                            FlutterRemix.bar_chart_2_line,
-                            color: kbluePrimaryColor,
-                          ),
-                          WidthWidget(8.0),
-                          NormalText(
-                            'Volume ',
-                            fontSize: kDefaultFontSize + 2,
-                            fontWeight: FontWeight.bold,
-                            color: kgreyTextColor,
-                          ),
-                        ],
-                      ),
-                      NepseStockModel.fromStorage() == null
-                          ? const NormalText("")
-                          : NormalText(
-                              kmbGenerator(NepseStockModel.fromStorage()!
-                                          .volumeTraded[
-                                      NepseStockModel.fromStorage()!
-                                              .volumeTraded
-                                              .length -
-                                          1])
-                                  .toString(),
-                              fontSize: kDefaultFontSize + 2,
-                              fontWeight: FontWeight.bold,
-                            )
-                    ],
-                  ),
-                  const HeightWidget(kDefaultFontSize),
-                  const NormalText(
-                    "About",
-                    fontSize: kDefaultFontSize + 6,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  const HeightWidget(16.0),
-                  Consumer(
-                    builder: (context, ref, child) {
-                      final readMore = ref.watch(nepseProvider).readMore;
-                      final descriptionProvider = ref.watch(nepseProvider);
-                      return Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          NormalText(
-                            readMore
-                                ? descriptionProvider.nepseLongDescription
-                                : descriptionProvider.nepseShortDescription,
-                            color: kgreyTextColor,
-                            fontWeight: FontWeight.w500,
-                          ),
-                          const HeightWidget(8.0),
-                          GestureDetector(
-                            onTap: () {
-                              ref.read(nepseProvider).readMoreToggled();
-                            },
-                            child: NormalText(
-                              readMore ? "Read Less" : "Read More",
-                              color: kbluePrimaryColor,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          )
-                        ],
-                      );
-                    },
-                  )
                 ],
               ),
             ),
---
+++ lib/src/features/homepage/widgets/nepse_description.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_remix/flutter_remix.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:upstock/src/features/homepage/bloc/home_page_notifier.dart';
+
+import '../../../common/constants/constants.dart';
+import '../../../common/widgets/size/custom_size_widget.dart';
+import '../../../common/widgets/text/custom_normal_text_widget.dart';
+import '../models/nepse_stock_model.dart';
+
+class NepseDescription extends StatefulWidget {
+  const NepseDescription({Key? key}) : super(key: key);
+
+  @override
+  State<NepseDescription> createState() => _NepseDescriptionState();
+}
+
+class _NepseDescriptionState extends State<NepseDescription> {
+  String kmbGenerator(String num) {
+    int num1 = double.parse(num).toInt();
+    if (num1 > 999 && num1 < 99999) {
+      return "${(num1 / 1000).toStringAsFixed(1)} K";
+    } else if (num1 > 99999 && num1 < 999999) {
+      return "${(num1 / 1000).toStringAsFixed(0)} K";
+    } else if (num1 > 999999 && num1 < 999999999) {
+      return "${(num1 / 1000000).toStringAsFixed(1)} M";
+    } else if (num1 > 999999999) {
+      return "${(num1 / 1000000000).toStringAsFixed(1)} B";
+    } else {
+      return num1.toString();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const NormalText(
+              "Market Stats",
+              fontSize: kDefaultFontSize + 6,
+              fontWeight: FontWeight.bold,
+            ),
+            const HeightWidget(8.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Icon(
+                      FlutterRemix.funds_box_line,
+                      color: kbluePrimaryColor,
+                    ),
+                    WidthWidget(8.0),
+                    NormalText(
+                      'Nepse Index ',
+                      fontSize: kDefaultFontSize + 2,
+                      fontWeight: FontWeight.bold,
+                      color: kgreyTextColor,
+                    ),
+                  ],
+                ),
+                const HeightWidget(kDefaultFontSize * 2),
+                NepseStockModel.fromStorage() == null
+                    ? const NormalText("")
+                    : NormalText(
+                        NepseStockModel.fromStorage()!
+                            .closingPrice[NepseStockModel.fromStorage()!
+                                    .closingPrice
+                                    .length -
+                                1]
+                            .toString(),
+                        fontSize: kDefaultFontSize + 2,
+                        fontWeight: FontWeight.bold,
+                      )
+              ],
+            ),
+            const HeightWidget(8.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Icon(
+                      FlutterRemix.bar_chart_2_line,
+                      color: kbluePrimaryColor,
+                    ),
+                    WidthWidget(8.0),
+                    NormalText(
+                      'Volume ',
+                      fontSize: kDefaultFontSize + 2,
+                      fontWeight: FontWeight.bold,
+                      color: kgreyTextColor,
+                    ),
+                  ],
+                ),
+                NepseStockModel.fromStorage() == null
+                    ? const NormalText("")
+                    : NormalText(
+                        kmbGenerator(NepseStockModel.fromStorage()!
+                                .volumeTraded[NepseStockModel.fromStorage()!
+                                    .volumeTraded
+                                    .length -
+                                1])
+                            .toString(),
+                        fontSize: kDefaultFontSize + 2,
+                        fontWeight: FontWeight.bold,
+                      )
+              ],
+            ),
+            const HeightWidget(kDefaultFontSize),
+            const NormalText(
+              "About",
+              fontSize: kDefaultFontSize + 6,
+              fontWeight: FontWeight.bold,
+            ),
+            const HeightWidget(16.0),
+            Consumer(
+              builder: (context, ref, child) {
+                final readMore = ref.watch(nepseProvider).readMore;
+                final descriptionProvider = ref.watch(nepseProvider);
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    NormalText(
+                      readMore
+                          ? descriptionProvider.nepseLongDescription
+                          : descriptionProvider.nepseShortDescription,
+                      color: kgreyTextColor,
+                      fontWeight: FontWeight.w500,
+                    ),
+                    const HeightWidget(8.0),
+                    GestureDetector(
+                      onTap: () {
+                        ref.read(nepseProvider).readMoreToggled();
+                      },
+                      child: NormalText(
+                        readMore ? "Read Less" : "Read More",
+                        color: kbluePrimaryColor,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    )
+                  ],
+                );
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}

5c36edf36db5101c32e2b2f644627cf120e39a63
unknown <nareshkch27@gmail.com>
2022-06-01 13:07:32 +0545

    Company Model Created

---
+++ lib/src/features/stock_details/models/company_model.freezed.dart
@@ -0,0 +1,260 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'company_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+CompanyModel _$CompanyModelFromJson(Map<String, dynamic> json) {
+  return _CompanyModel.fromJson(json);
+}
+
+/// @nodoc
+class _$CompanyModelTearOff {
+  const _$CompanyModelTearOff();
+
+  _CompanyModel call(
+      {required String symbol,
+      @JsonKey(name: "full_name") required String fullName,
+      required String description,
+      required String exchange,
+      required String type}) {
+    return _CompanyModel(
+      symbol: symbol,
+      fullName: fullName,
+      description: description,
+      exchange: exchange,
+      type: type,
+    );
+  }
+
+  CompanyModel fromJson(Map<String, Object?> json) {
+    return CompanyModel.fromJson(json);
+  }
+}
+
+/// @nodoc
+const $CompanyModel = _$CompanyModelTearOff();
+
+/// @nodoc
+mixin _$CompanyModel {
+  String get symbol => throw _privateConstructorUsedError;
+  @JsonKey(name: "full_name")
+  String get fullName => throw _privateConstructorUsedError;
+  String get description => throw _privateConstructorUsedError;
+  String get exchange => throw _privateConstructorUsedError;
+  String get type => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $CompanyModelCopyWith<CompanyModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $CompanyModelCopyWith<$Res> {
+  factory $CompanyModelCopyWith(
+          CompanyModel value, $Res Function(CompanyModel) then) =
+      _$CompanyModelCopyWithImpl<$Res>;
+  $Res call(
+      {String symbol,
+      @JsonKey(name: "full_name") String fullName,
+      String description,
+      String exchange,
+      String type});
+}
+
+/// @nodoc
+class _$CompanyModelCopyWithImpl<$Res> implements $CompanyModelCopyWith<$Res> {
+  _$CompanyModelCopyWithImpl(this._value, this._then);
+
+  final CompanyModel _value;
+  // ignore: unused_field
+  final $Res Function(CompanyModel) _then;
+
+  @override
+  $Res call({
+    Object? symbol = freezed,
+    Object? fullName = freezed,
+    Object? description = freezed,
+    Object? exchange = freezed,
+    Object? type = freezed,
+  }) {
+    return _then(_value.copyWith(
+      symbol: symbol == freezed
+          ? _value.symbol
+          : symbol // ignore: cast_nullable_to_non_nullable
+              as String,
+      fullName: fullName == freezed
+          ? _value.fullName
+          : fullName // ignore: cast_nullable_to_non_nullable
+              as String,
+      description: description == freezed
+          ? _value.description
+          : description // ignore: cast_nullable_to_non_nullable
+              as String,
+      exchange: exchange == freezed
+          ? _value.exchange
+          : exchange // ignore: cast_nullable_to_non_nullable
+              as String,
+      type: type == freezed
+          ? _value.type
+          : type // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$CompanyModelCopyWith<$Res>
+    implements $CompanyModelCopyWith<$Res> {
+  factory _$CompanyModelCopyWith(
+          _CompanyModel value, $Res Function(_CompanyModel) then) =
+      __$CompanyModelCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {String symbol,
+      @JsonKey(name: "full_name") String fullName,
+      String description,
+      String exchange,
+      String type});
+}
+
+/// @nodoc
+class __$CompanyModelCopyWithImpl<$Res> extends _$CompanyModelCopyWithImpl<$Res>
+    implements _$CompanyModelCopyWith<$Res> {
+  __$CompanyModelCopyWithImpl(
+      _CompanyModel _value, $Res Function(_CompanyModel) _then)
+      : super(_value, (v) => _then(v as _CompanyModel));
+
+  @override
+  _CompanyModel get _value => super._value as _CompanyModel;
+
+  @override
+  $Res call({
+    Object? symbol = freezed,
+    Object? fullName = freezed,
+    Object? description = freezed,
+    Object? exchange = freezed,
+    Object? type = freezed,
+  }) {
+    return _then(_CompanyModel(
+      symbol: symbol == freezed
+          ? _value.symbol
+          : symbol // ignore: cast_nullable_to_non_nullable
+              as String,
+      fullName: fullName == freezed
+          ? _value.fullName
+          : fullName // ignore: cast_nullable_to_non_nullable
+              as String,
+      description: description == freezed
+          ? _value.description
+          : description // ignore: cast_nullable_to_non_nullable
+              as String,
+      exchange: exchange == freezed
+          ? _value.exchange
+          : exchange // ignore: cast_nullable_to_non_nullable
+              as String,
+      type: type == freezed
+          ? _value.type
+          : type // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$_CompanyModel implements _CompanyModel {
+  const _$_CompanyModel(
+      {required this.symbol,
+      @JsonKey(name: "full_name") required this.fullName,
+      required this.description,
+      required this.exchange,
+      required this.type});
+
+  factory _$_CompanyModel.fromJson(Map<String, dynamic> json) =>
+      _$$_CompanyModelFromJson(json);
+
+  @override
+  final String symbol;
+  @override
+  @JsonKey(name: "full_name")
+  final String fullName;
+  @override
+  final String description;
+  @override
+  final String exchange;
+  @override
+  final String type;
+
+  @override
+  String toString() {
+    return 'CompanyModel(symbol: $symbol, fullName: $fullName, description: $description, exchange: $exchange, type: $type)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _CompanyModel &&
+            (identical(other.symbol, symbol) || other.symbol == symbol) &&
+            (identical(other.fullName, fullName) ||
+                other.fullName == fullName) &&
+            (identical(other.description, description) ||
+                other.description == description) &&
+            (identical(other.exchange, exchange) ||
+                other.exchange == exchange) &&
+            (identical(other.type, type) || other.type == type));
+  }
+
+  @override
+  int get hashCode =>
+      Object.hash(runtimeType, symbol, fullName, description, exchange, type);
+
+  @JsonKey(ignore: true)
+  @override
+  _$CompanyModelCopyWith<_CompanyModel> get copyWith =>
+      __$CompanyModelCopyWithImpl<_CompanyModel>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_CompanyModelToJson(this);
+  }
+}
+
+abstract class _CompanyModel implements CompanyModel {
+  const factory _CompanyModel(
+      {required String symbol,
+      @JsonKey(name: "full_name") required String fullName,
+      required String description,
+      required String exchange,
+      required String type}) = _$_CompanyModel;
+
+  factory _CompanyModel.fromJson(Map<String, dynamic> json) =
+      _$_CompanyModel.fromJson;
+
+  @override
+  String get symbol;
+  @override
+  @JsonKey(name: "full_name")
+  String get fullName;
+  @override
+  String get description;
+  @override
+  String get exchange;
+  @override
+  String get type;
+  @override
+  @JsonKey(ignore: true)
+  _$CompanyModelCopyWith<_CompanyModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
